c0221989d6acf4fc4fe1fb2e327e850a8f0ceea4
pandu <pandu@dinustek.com>
2018-11-22 14:42:07 +0700

    search

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -11,14 +11,26 @@
 @section('js')
 <script>
  $('#tabel_karyawan').on('click','.pagination a', function(e)
+{
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    var url = $(this).attr('href');
+    $.get(url, function(tabel)
     {
-        e.preventDefault();
-        e.stopImmediatePropagation();
-        var url = $(this).attr('href');
-        $.get(url, function(tabel)
-        {
-            $('#tabel_karyawan').html(tabel);
-        });
+        $('#tabel_karyawan').html(tabel);
     });
+});
+
+$('#tabel_karyawan').on('submit','#cari',function(e){
+    e.preventDefault();
+    $.ajax({
+        url : $(this).attr('action'),
+        data:$(this).serialize(),
+        type : 'get',
+        success:function(html){
+            $('#tabel_karyawan').html(html);
+        }
+    });
+}); 
 </script>
 @endsection
